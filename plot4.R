d154623596fcc3305ad75286257c53b44b468c67
gotnosocks <rbc6zf@gmail.com>
2014-09-07 11:42:04 -0400

    Update plot4 to remove border on legend

--- plot4.R
+++ plot4.R
@@ -75,7 +75,8 @@ plot4 <- function() {
     lines(data$DateTime, data$Sub_metering_2, col = "red")
     lines(data$DateTime, data$Sub_metering_3, col = "blue")
     
-    legend("topright", 
+    legend("topright",
+           bty = "n",
            lty = c(1,1,1),
            col = c("black", "red", "blue"), 
            legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
